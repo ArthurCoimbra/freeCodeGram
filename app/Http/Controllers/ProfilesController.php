6c7807c56662e968cea840d6442a307a91a3fd72
ArthurCoimbra <arthurplank22@gmail.com>
2020-01-09 17:04:52 -0300

    Added Edit Profile.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,8 +9,25 @@ class ProfilesController extends Controller
 {
     public function index(\App\User $user)
     {
-        return view('profiles/index', [
-          'user' => $user,
+        return view('profiles/index', compact('user'));
+    }
+
+    public function edit(\App\User $user)
+    {
+        return view('profiles/edit', compact('user'));
+    }
+
+    public function update(\App\User $user)
+    {
+        $data = request()->validate([
+          'title' => 'required',
+          'description' => 'required',
+          'url' => 'url',
+          'image' => '',
         ]);
+
+        auth()->user()->profile->update($data);
+
+        return redirect("/profile/{$user->id}");
     }
 }
